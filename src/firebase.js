68d03939e89a83041114c244af012d144d189289
sanjay10985 <sanjaytomar717@gmail.com>
2022-10-03 12:47:14 +0530

    All the implementation of authentication in the app is done from the basic

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdoy1LFAVJyE431dGhuemNK2QJ2-5Tzxk",
+  authDomain: "chat-app-9c4ef.firebaseapp.com",
+  projectId: "chat-app-9c4ef",
+  storageBucket: "chat-app-9c4ef.appspot.com",
+  messagingSenderId: "617251180042",
+  appId: "1:617251180042:web:dfd80689f73d5a34e5d9f3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
