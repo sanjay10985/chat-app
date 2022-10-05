afacc16781d31800e61860de283a8f66e0083356
sanjay10985 <sanjaytomar717@gmail.com>
2022-10-05 10:22:29 +0530

    Full app is complete with the send and receive

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React from "react";
+import { auth } from "../firebase";
 
 const style = {
-     message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
-     name: `fixed mt-[-4rem] text-gray-600 text-xs`,
-     sent: `bg-[#395dff] text-white flex-row-reverse text-row-reverse text-end float-right rounded-bl-full`,
-     received: `bg-[#e5e5ea] text-black float-left rounded-br-full`,
-}
+  message: `flex items-center shadow-xl m-4 py-2 px-3 rounded-tl-full rounded-tr-full`,
+  name: `absolute mt-[-4rem] text-gray-600 text-xs`,
+  sent: `bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full`,
+  received: `bg-[#e5e5ea] text-black float-left rounded-br-full`,
+};
 
-function Message() {
+function Message({ message }) {
+  const messageClass = 
+    message.uid === auth.currentUser.uid
+      ? `${style.sent}`
+      : `${style.received}`;
+
   return (
     <div>
-        <div className={style.message}>
-            <p className={style.name}> Dave</p>
-            <p>I am learning React!</p>
-        </div>
+      <div className={`${style.message} ${messageClass}`}>
+        <p className={style.name}> {message.name}</p>
+        <p>{message.text}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Message
+export default Message;
